ff88c9cf3d6f78ed4e4bf6e25a6bde570dc5c446
Yogesh Sahu <47670619+yogesh-sahu@users.noreply.github.com>
2024-06-17 14:33:38 +0530

    gps migration

---
+++ server/migrations/2024_06_17_053457_alter_vehicle_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+
+            $table->string('gps')->nullable();
+            $table->string('erp')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+
+
+            $table->dropColumn('gps');
+            $table->dropColumn('erp');
+        });
+    }
+};
